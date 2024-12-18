43deaa9baeb0b7173c15dc19018d87eff728ed23
Roni Stiawan <roni.physics@gmail.com>
2024-12-18 08:43:28 +0700

    fix some pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span><img src='https://res.cloudinary.com/dmhtdcvan/image/upload/v1725097352/Varmasi/varmasi.png' alt='logo'/></span>,
-  // project: {
-  //   link: 'https://github.com/ronistiawan/varmasi-docs',
-  // },
-  // chat: {
-  //   link: 'https://discord.com',
-  // },
-  // docsRepositoryBase: 'https://github.com/ronistiawan/varmasi-docs',
+  
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Varmasi'
+      }
+    }
+  },
   editLink: { text: ''},
   feedback: { content: '', labels: '' },
 
   footer: {
     text: 'Varmasi @ 2024'
-  },
+  }
 }
 
 export default config
